89b4c062de9e787b7659e5f5d23f5a261c051d34
Stefano Zanelli <stefano.zanelli@telvox.com>
2016-02-09 17:21:25 +0100

    Refactored as tomcat webapp project and added  POST  service

--- JerseySpike/src/main/java/zannik/rest/HelloWorldService.java
+++ JerseySpike/src/main/java/zannik/rest/HelloWorldService.java
@@ -1,17 +1,25 @@
 package zannik.rest;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
+
+import javax.ws.rs.*;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.MediaType;
 
 @Path("/hello")
 public class HelloWorldService {
+
     @GET
     @Path("/{param}")
-    @Produces("text/plain")
+    //@Produces(MediaType.APPLICATION_JSON)
     public Response getMessage(@PathParam("param") String message) {
         String output = "Jersey says " + message;
+        return Response.status(200).entity(output).build();
+    }
+
+    @POST
+    @Path("/login")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response postCredentials(MyUser user) {
+        String output = "Benvenuto " + user.UserName;
         return Response.status(200).entity(output).build();
     }
 }
---
+++ JerseySpike/src/main/java/zannik/rest/MyUser.java
@@ -0,0 +1,9 @@
+package zannik.rest;
+
+/**
+ * Created by admin on 08/02/2016.
+ */
+public class MyUser {
+    public String UserName;
+    public String Password;
+}
